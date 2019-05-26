4e70bd0fb101a99cb7e236a69033942849338856
Krid Paisalwatchrakij <pakingcho@gmail.com>
2019-05-26 10:51:29 +0700

    minor

---
+++ Assets/Scripts/GamePlay/SoldierSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierSpawner : MonoBehaviour
+{
+    public static SoldierSpawner Instance { get; private set; }
+
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+
+        if (!spawnPoint)
+            spawnPoint = GameObject.Find("SpawnSoldierPoint").transform;
+    }
+
+    public void SpawnSoldier(GameObject soldier)
+    {
+        Vector3 randomSpawnPoint = new Vector3(spawnPoint.position.x, spawnPoint.position.y + Random.Range(-0.2f, 0.2f));
+        GameObject tempSoldier = Instantiate(soldier, randomSpawnPoint, spawnPoint.rotation);
+
+        SetSpriteOrder(tempSoldier);
+    }
+
+    void SetSpriteOrder(GameObject soldier)
+    {
+        soldier.GetComponent<SpriteRenderer>().sortingOrder = (int)(soldier.transform.position.y * -100);
+    }
+}
---
+++ Assets/Scripts/GamePlay/Unit/SoldierBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierBase : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        body.velocity = new Vector2(speed, 0);
+    }
+}
